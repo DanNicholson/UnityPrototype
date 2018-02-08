f89c76573320754fe524ab299bd342ab8456b507
Daniel Nicholson <32296527+DanNicholson@users.noreply.github.com>
2018-02-08 08:55:06 +0000

    built unity project -  began creating controller

---
+++ New Unity Project/Assets/Scripts/Controller2D.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+	const float skinWidth = .015f;
+
+	BoxCollider2D collider;
+	RayCastOrigins raycastOrigins;
+
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<BoxCollider2D>();
+	}
+
+	void UpdateRayCastOrigins(){
+		Bounds bounds = collider.bounds;
+		bounds.Expand (skinWidth * -2);
+
+		raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+		raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+		raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+		raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+	}
+
+	//This will hold positions for our RayCasts
+	struct RayCastOrigins {
+		public Vector2 topLeft, topRight;
+		public Vector2 bottomLeft, bottomRight;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ New Unity Project/Assets/Scripts/TestPlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerMovement : MonoBehaviour {
+
+	public float playerSpeed = 10;
+	public float jumpHeight = 15;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey(KeyCode.D)){
+			GetComponent<Rigidbody2D>().transform.position += Vector3.right * playerSpeed * Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.A)){
+			GetComponent<Rigidbody2D>().transform.position += Vector3.left * playerSpeed * Time.deltaTime;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Jump();
+		}
+
+	}
+
+	void Jump(){
+		//GetComponent<Rigidbody2D>().transform.position += Vector3.up * jumpHeight * Time.deltaTime;
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+	}
+}
